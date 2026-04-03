8f3a824e0d3d377267b03f42d6a3327a03d3b4b7
André Mafioletti <and.mafioletti@gmail.com>
2026-04-03 15:58:06 -0300

    fix: adiciona url da vercel no cors e libera credentials para o clerk

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,7 +8,8 @@ const port = process.env.PORT || 3001;
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000', 'https://projeto-agendamentos-ashen.vercel.app'],
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
