c61798089a94e2c37ccaf6861c7284acb105d3b9
André Mafioletti <and.mafioletti@gmail.com>
2026-04-21 10:31:26 -0300

    feat: integra Sentry para observabilidade (RNF-04)

---
+++ apps/frontend/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://10ae453020957acf840ae74766fb5a3f@o4511258036142080.ingest.us.sentry.io/4511258043088896",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
